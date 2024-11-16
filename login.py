922b7433bd854034e61a8469812d5993a1b9d534
Erik Andres <erikqtrs@gmail.com>
2024-11-15 23:51:19 -0500

    I have added some validation to login

--- login.py
+++ login.py
@@ -1,3 +1,8 @@
 print('login')
 username = input('Insert your username')
 password = input('Insert your password')
+
+if username != 'admin' and password != '1234':
+    print('Your username or password is incorrect')
+else:
+    print('You\'ve been logged in')
